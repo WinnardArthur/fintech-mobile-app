acaaf5aaa7979cf4efc6186cc0ff988a5de6c42e
WinnardArthur <winnardarthur7@gmail.com>
2024-06-04 09:30:22 +0000

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#3D38ED",
+        "primary-muted": "#C9C8FA",
+        background: "#F5F5F5",
+        dark: "#141518",
+        gray: "#626D77",
+        "light-gray": "#D8DCE2",
+      },
+    },
   },
   plugins: [],
 };
