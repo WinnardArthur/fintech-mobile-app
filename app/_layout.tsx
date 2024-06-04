9f5e3e7703b4a07c2e1c9ec2b838368189000a7f
WinnardArthur <winnardarthur7@gmail.com>
2024-06-04 09:32:56 +0000

    restructure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,9 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
 export {
@@ -18,7 +14,7 @@ export {
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
+const InitialRootLayout = () => {
   const [loaded, error] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
@@ -39,13 +35,21 @@ export default function RootLayout() {
     return null;
   }
 
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
     </Stack>
   );
-}
+};
+
+const RootLayout = () => {
+  return (
+    <>
+      <StatusBar style="light" />
+      <InitialRootLayout />
+    </>
+  );
+};
+
+export default RootLayout;
