f8c416a779eb2c7c27f106194d406a9fe5631431
WinnardArthur <winnardarthur7@gmail.com>
2024-06-04 09:31:14 +0000

    create sign up screen

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,83 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import React, { useState } from "react";
+
+import { defaultStyles } from "@/constants/Styles";
+import Colors from "@/constants/Colors";
+import { Link } from "expo-router";
 
 const SignUpPage = () => {
-  return (
-    <View>
-      <Text>SignUpPage</Text>
-    </View>
-  )
-}
+  const [credentials, setCredentials] = useState({
+    countryCode: "+233",
+    phoneNumber: "",
+  });
 
-export default SignUpPage
+  const keyboardVerticalOffset = Platform.OS === "ios" ? 90 : 0;
+
+  // Handle Sign up
+  const handleSignUp = () => {};
+
+  return (
+    <KeyboardAvoidingView
+      className="flex-1"
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={keyboardVerticalOffset}
+    >
+      <View style={defaultStyles.container}>
+        <Text className="" style={defaultStyles.header}>
+          Let's get started!
+        </Text>
+        <Text>
+          Enter your phone number. We will send you a confirmation code there.
+        </Text>
+
+        <View className="my-[40] flex-row gap-x-2">
+          <TextInput
+            placeholder="Country code"
+            keyboardType="numeric"
+            className="bg-gray-300 block w-24 p-[18] rounded-[16px] text-[20px]"
+            placeholderTextColor={Colors.gray}
+            value={credentials.countryCode}
+          />
+          <TextInput
+            placeholder="Mobile number"
+            keyboardType="numeric"
+            className="bg-gray-300 flex-1 p-[18] rounded-[16px] text-[20px]"
+            placeholderTextColor={Colors.gray}
+            value={credentials.phoneNumber}
+            onChangeText={(e) =>
+              setCredentials((prev) => ({ ...prev, phoneNumber: e }))
+            }
+          />
+        </View>
+
+        <Link href="/login" asChild replace>
+          <TouchableOpacity activeOpacity={0.7}>
+            <Text style={defaultStyles.textLink} className="text-center">
+              Already have an account? Log in
+            </Text>
+          </TouchableOpacity>
+        </Link>
+
+        <TouchableOpacity
+          style={[defaultStyles.pillButton]}
+          className={`mt-auto ${
+            !!credentials.phoneNumber ? "bg-dark" : "bg-gray"
+          }`}
+          disabled={!credentials.phoneNumber}
+          activeOpacity={0.7}
+        >
+          <Text style={[defaultStyles.buttonText]}>Sign up</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default SignUpPage;
