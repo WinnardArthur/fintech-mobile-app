58df85348b4b02c00ec46dae48b2fb4a4e10533a
WinnardArthur <winnardarthur7@gmail.com>
2024-06-06 10:37:35 +0000

    add balance store

--- app/(authenticated)/(tabs)/home/index.tsx
+++ app/(authenticated)/(tabs)/home/index.tsx
@@ -2,16 +2,32 @@ import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import { RoundButton } from "@/components/round-button";
 import { Dropdown } from "@/components/ui/dropdown";
+import { balanceStore } from "@/store/balance-store";
+import { defaultStyles } from "@/constants/Styles";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "@/constants/Colors";
 
 const balance = 1820;
 
 const Homepage = () => {
-  const onAddMoney = () => {};
+  const { balance, runTransaction, transactions, clearTransactions } =
+    balanceStore();
+
+  // Add money function
+  const onAddMoney = () => {
+    runTransaction({
+      id: Math.random().toString(),
+      amount: Math.floor(Math.random() * 1000) * (Math.random() > 0.5 ? 1 : -1),
+      date: new Date(),
+      title: "Added money",
+    });
+  };
+
   return (
     <ScrollView className="">
       <View className="m-[80] items-center">
         <View className="flex-row items-baseline justify-center gap-[10]">
-          <Text className="text-[60px] font-bold">{balance}</Text>
+          <Text className="text-[60px] font-bold">{balance()}</Text>
           <Text className="text-[30px] font-semibold">$</Text>
         </View>
       </View>
@@ -24,9 +40,43 @@ const Homepage = () => {
             onAddMoney();
           }}
         />
-        <RoundButton icon={"refresh"} text="Exchange" />
+        <RoundButton
+          icon={"refresh"}
+          text="Exchange"
+          onPress={clearTransactions}
+        />
         <RoundButton icon={"list"} text="Details" />
         <Dropdown />
+      </View>
+
+      <Text style={defaultStyles.sectionHeader}>Transactions</Text>
+      <View className="mx-[20] p-[14] bg-white rounded-[16px]">
+        {transactions.length === 0 && (
+          <Text className="text-gray mx-[20]">No transactions yet</Text>
+        )}
+        <View className="gap-y-4">
+          {transactions.map((transaction) => (
+            <View
+              key={transaction.id}
+              className="flex-row items-center gap-x-[20]"
+            >
+              <View className="w-[40] h-[40] rounded-full bg-light-gray justify-center items-center">
+                <Ionicons
+                  name={transaction.amount > 0 ? "add" : "remove"}
+                  size={22}
+                  color={Colors.dark}
+                />
+              </View>
+              <View className="flex-1">
+                <Text className="font-normal">{transaction.title}</Text>
+                <Text className="text-gray text-[12px]">
+                  {transaction.date.toLocaleString()}
+                </Text>
+              </View>
+              <Text>{transaction.amount}$</Text>
+            </View>
+          ))}
+        </View>
       </View>
     </ScrollView>
   );
---
+++ store/balance-store.ts
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import { zustandStorage } from "./mmkv-storage";
+
+export interface Transaction {
+  id: string;
+  amount: number;
+  date: Date;
+  title: string;
+}
+
+export interface BalanceState {
+  transactions: Array<Transaction>;
+  runTransaction: (transaction: Transaction) => void;
+  balance: () => number;
+  clearTransactions: () => void;
+}
+
+export const balanceStore = create<BalanceState>()(
+  persist(
+    (set, get) => ({
+      transactions: [],
+      runTransaction: (transaction: Transaction) => {
+        set((state) => ({
+          transactions: [transaction, ...state.transactions],
+        }));
+      },
+      balance: () =>
+        get().transactions.reduce((acc, val) => acc + val.amount, 0),
+      clearTransactions: () => {
+        set({ transactions: [] });
+      },
+    }),
+    {
+      name: "balance",
+      storage: createJSONStorage(() => zustandStorage),
+    }
+  )
+);
