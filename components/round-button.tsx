07526d1717a970aae8b1ce0fd6d7336dee1fd718
WinnardArthur <winnardarthur7@gmail.com>
2024-06-06 06:27:34 +0000

    create round-button component

---
+++ components/round-button.tsx
@@ -0,0 +1,26 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
+import Colors from "@/constants/Colors";
+
+interface RoundButtonProps {
+  text: string;
+  icon: typeof Ionicons.defaultProps;
+  onPress?: () => void;
+}
+
+export const RoundButton = ({ text, icon, onPress }: RoundButtonProps) => {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.4}
+      className="items-center gap-[10]"
+      onPress={onPress}
+    >
+      <View className="w-[60] h-[60] rounded-full bg-light-gray justify-center items-center">
+        <Ionicons name={icon} size={30} color={Colors.dark} />
+      </View>
+      <Text className="text-[16px] font-medium text-dark">{text}</Text>
+    </TouchableOpacity>
+  );
+};
