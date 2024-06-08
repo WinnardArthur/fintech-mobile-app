720d008f3a9bdba8e248ac56a34842ddec7b2a78
WinnardArthur <winnardarthur7@gmail.com>
2024-06-08 09:07:51 +0000

    restructure crypto tab screen

--- app/(authenticated)/(tabs)/_layout.tsx
+++ app/(authenticated)/(tabs)/_layout.tsx
@@ -1,32 +1,39 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { Tabs } from "expo-router";
+import { Tabs, useSegments } from "expo-router";
 import { FontAwesome } from "@expo/vector-icons";
 import Colors from "@/constants/Colors";
 import { BlurView } from "expo-blur";
 import { CustomHeader } from "@/components/custom-header";
 
 const TabsLayout = () => {
+  const segments = useSegments();
+
+  const isCryptoIdPage = segments.includes("[cryptoId]");
+
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors.primary,
-        tabBarBackground: () => (
-          <BlurView
-            intensity={120}
-            tint="extraLight"
-            style={{ flex: 1, backgroundColor: "rgba(0,0,0,0.2)" }}
-          />
-        ),
-        tabBarStyle: {
-          backgroundColor: "transparent",
-          position: "absolute",
-          bottom: 0,
-          left: 0,
-          right: 0,
-          elevation: 0,
-          borderTopWidth: 0,
-        },
+      screenOptions={({ route }) => {
+        return {
+          tabBarActiveTintColor: Colors.primary,
+          tabBarBackground: () => (
+            <BlurView
+              intensity={120}
+              tint="extraLight"
+              style={{ flex: 1, backgroundColor: "rgba(0,0,0,0.2)" }}
+            />
+          ),
+          tabBarStyle: {
+            backgroundColor: "transparent",
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            elevation: 0,
+            borderTopWidth: 0,
+            display: isCryptoIdPage ? "none" : "flex",
+          },
+        };
       }}
     >
       <Tabs.Screen
@@ -65,6 +72,8 @@ const TabsLayout = () => {
           tabBarIcon: ({ size, color }) => (
             <FontAwesome name="bitcoin" size={size} color={color} />
           ),
+          header: () => <CustomHeader />,
+          headerShown: !isCryptoIdPage,
         }}
       />
       <Tabs.Screen
